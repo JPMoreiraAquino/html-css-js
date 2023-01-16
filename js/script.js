f45cc71e53111bd8e57a72bb762872a4c0d83f00
JPMoreira <jp02120123@gmail.com>
2023-01-16 17:42:38 -0300

    feat: add screen login

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.getElementById("submit-button").addEventListener("click", function(event){
+  event.preventDefault()
+
+  console.log('jp')
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  //validate the username and password, and handle login accordingly
+});
