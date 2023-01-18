c7e610c9e4e824f2972cf04cbfa87fb63fe2ee52
JPMoreira <jp02120123@gmail.com>
2023-01-18 15:04:54 -0300

    feat: add script js for sreen login

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
-document.getElementById("submit-button").addEventListener("click", function(event){
-  event.preventDefault()
-
-  console.log('jp')
-  var username = document.getElementById("username").value;
-  var password = document.getElementById("password").value;
-  //validate the username and password, and handle login accordingly
-});
+let inputs = document.getElementsByClassName('input-form');
+for(let input of inputs) {
+  input.addEventListener('blur', () => {
+    if(input.value.trim() != "") {
+      input.classList.add('has-val')
+    } else {
+      input.classList.remove('has-val')
+    }
+  })
+} 
